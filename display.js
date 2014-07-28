d8ab4fb617e15ab75d5f96f4f916c720696f878e
shaunmulligan <switchflip@gmail.com>
2014-07-28 20:03:13 +0100

    displayNumber function shows floating point numbers on disply

--- display.js
+++ display.js
@@ -1,12 +1,25 @@
 //#######################################
 var MAX7219 = require('max7219');
-
 var disp = new MAX7219("/dev/spidev0.0");
 disp.setDecodeAll();
 disp.setScanLimit(8);
 disp.startup();
-disp.setDigitSymbol(0, "R");
-disp.setDigitSymbol(1, "E");
-disp.setDigitSymbol(2, "S");
-disp.setDigitSymbol(3, "i");
-disp.setDigitSymbol(4, "N");
+disp.clearDisplay(function() {
+    displayNumber(1.2, function(){});
+});
+
+function displayNumber(number,callback) {
+    sNumber = number.toString();
+
+    for (var i = 0, len = sNumber.length; i < len; i += 1) {
+        var digit = len-1-i;
+        if (sNumber[digit] == '.'){
+            disp.setDigitSymbol(i, sNumber[digit-1],true);
+            len = len-1;
+
+        }else{
+            disp.setDigitSymbol(i, sNumber[digit]);
+        }
+}
+    callback();
+}
