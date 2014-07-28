61c90d036ea80142b97b1dc06b0354fd36e1a90f
shaunmulligan <switchflip@gmail.com>
2014-07-28 17:15:08 +0100

    commented out broken spi code

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ var app = tty.createServer({
 
 app.listen();
 //#######################################
-var MAX7219 = require('max7219');
+// var MAX7219 = require('max7219');
 
-var disp = new MAX7219("/dev/spidev1.0");
-disp.setDecodeAll();
-disp.setScanLimit(8);
-disp.startup();
-disp.setDigitSymbol(0, "H");
-disp.setDigitSymbol(1, "E");
-disp.setDigitSymbol(2, "L");
-disp.setDigitSymbol(3, "P");
+// var disp = new MAX7219("/dev/spidev1.0");
+// disp.setDecodeAll();
+// disp.setScanLimit(8);
+// disp.startup();
+// disp.setDigitSymbol(0, "H");
+// disp.setDigitSymbol(1, "E");
+// disp.setDigitSymbol(2, "L");
+// disp.setDigitSymbol(3, "P");
