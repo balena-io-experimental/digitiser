88290b2fc6b56c940751d8efae352b1d835d02ad
shaunmulligan <switchflip@gmail.com>
2014-07-28 16:21:15 +0100

    initial commit, with tty.js terminal enabled

---
+++ readme.md
@@ -0,0 +1,1 @@
+Digitiser is a resin.io powered application that displays the number of currently connected devices on resin.io using a 8 digit 7-segment display.
---
+++ server.js
@@ -0,0 +1,11 @@
+var tty = require('tty.js');
+// Simple tty.js in app mode
+var app = tty.createServer({
+  shell: 'bash',
+  users: {
+    admin: 'admin'
+  },
+  port: process.env.PORT || '8080'
+});
+
+app.listen();
